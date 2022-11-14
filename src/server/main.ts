5016165ebe7408c89ab6352e2d8f47a4572a708b
VASAPOL RITTIDEAH <70092356+vasapol123@users.noreply.github.com>
2022-11-14 23:02:45 +0700

    change: change swagger ui path to `/docs`

--- src/server/main.ts
+++ src/server/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     .addTag('anonymous')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
 }
